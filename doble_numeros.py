2a293038eb267fea420ffdc3b7ee552bb2f9d279
HamletGonzalez <gonzalezhamlet7@gmail.com>
2024-08-13 16:52:27 -0400

    practica 7

---
+++ doble_numeros.py
@@ -0,0 +1,21 @@
+""""Doble todos los números en una lista: Dada una lista de números enteros, crea
+una función llamada double_numbers que tome esta lista como entrada y devuelva
+una nueva lista donde cada número se haya duplicado."""
+
+numeros = [1,2,3,4,5,6,7,8,9,10,11,12]
+
+def double_numbers(numeros):
+    x = lambda a : a*2
+
+    numeros_duplicados = []
+
+    for item in numeros:
+        numeros_duplicados.append(x(item))
+    
+    print('lista anterior:')
+    print(numeros)
+    print('lista nueva:')
+    print(numeros_duplicados)
+
+double_numbers(numeros)
+    
