32b37c3114ddd1144823fc3629909f20a91a633b
HamletGonzalez <gonzalezhamlet7@gmail.com>
2024-08-27 12:04:58 -0400

    git push

--- mayusculas.py
+++ mayusculas.py
@@ -6,7 +6,7 @@ lista donde todas las cadenas se conviertan a mayúsculas."""
 palabras = ["Python", "es", "un", "lenguaje", "de", "programación"]
 
 def uppercase_strings(palabras):
-    x = lambda a : a.upper()
+    x = lambda a : a.upper() 
     palabras_upper = map(x,palabras)
     print(list(palabras_upper))
 uppercase_strings(palabras)
